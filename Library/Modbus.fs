5c845e71427c4623a792c9c8ae75360de6d90fc9
Bradley Phillips <bradder555@users.noreply.github.com>
2019-04-02 09:52:06 +0800

    feat: reading and writing to the H-Register works
    The deserialization still needs test coverage, but now i am confident
    that the serialization is 100% correct, it was easy to identify what
    changes were required to fix the issues with reading and writing the
    holding registers

--- Library/Modbus.fs
+++ Library/Modbus.fs
@@ -2,7 +2,7 @@ module Modbus
 // for my code, the head should always be the least significant
 
 open ModbusTypes
-open System 
+open System
 open System.Threading
 open Util
 
@@ -15,7 +15,7 @@ let validateMbap (slaveId : byte) (frame : byte []) : PDU * Mbap =
   let (TidH :: TidL :: PiH :: PiL :: LH :: LL :: Ui :: Pdu) = frame
   let length = [LL; LH; 0uy; 0uy] |> tI32
   let lengthU = [LL; LH] |> tU16
-  let mbap : Mbap = 
+  let mbap : Mbap =
     {
       Length = lengthU
       ProtocolIdentifier = [PiL; PiH] |> tU16
@@ -23,87 +23,87 @@ let validateMbap (slaveId : byte) (frame : byte []) : PDU * Mbap =
       UnitIdentifier = Ui
     }
 
-  // verify the length 
+  // verify the length
   if (frame.Length - 6) <> length then
     FormatException("Frame is reporting a different length than received") |> raise
 
   if slaveId <> Ui then
     FormatException(sprintf "This slave is %i, requested slave is %i" slaveId Ui) |> raise
-  
+
   if PiL <> 0uy || PiH <> 0uy then
     FormatException("Protocol identifier is expected to be zero") |> raise
 
   Pdu, mbap
 
-let modError = 
+let modError =
   {
-    FunctionCode = Invalid 
-    ExceptionCode = IllegalFunction 
+    FunctionCode = Invalid
+    ExceptionCode = IllegalFunction
   } |> ModErrorReq
 
-let validatePdu (pdu : byte list) : Request = 
+let validatePdu (pdu : byte list) : Request =
   let (Fc :: OffsetH :: OffsetL :: Rem) = pdu
-  let functionCode = Fc |> ModbusTypes.FunctionCode.TryFromByte  
+  let functionCode = Fc |> ModbusTypes.FunctionCode.TryFromByte
   match functionCode with
-  | Ok functionCode -> 
+  | Ok functionCode ->
     match functionCode with
     | FunctionCode.Invalid -> modError
-    | FunctionCode.ReadDO -> 
-      ReadDoRequest.TryParse pdu 
-      |> function 
-         | Ok x -> x |> ReadDOReq 
+    | FunctionCode.ReadDO ->
+      ReadDoRequest.TryParse pdu
+      |> function
+         | Ok x -> x |> ReadDOReq
          | Error (p,e) -> modError
 
     | FunctionCode.ReadDI ->
       ReadDiRequest.TryParse pdu
-      |> function 
-         | Ok x -> x |> ReadDIReq 
+      |> function
+         | Ok x -> x |> ReadDIReq
          | Error (p,e) -> modError
 
     | FunctionCode.ReadHReg ->
-      ReadHRegRequest.TryParse pdu 
-      |> function 
-         | Ok x -> x |> ReadHRegReq 
+      ReadHRegRequest.TryParse pdu
+      |> function
+         | Ok x -> x |> ReadHRegReq
          | Error (p,e) -> modError
 
-    | FunctionCode.ReadIReg -> 
+    | FunctionCode.ReadIReg ->
       ReadIRegRequest.TryParse pdu
-      |> function 
-         | Ok x -> x |> ReadIRegReq 
-         | Error (p,e) -> modError 
+      |> function
+         | Ok x -> x |> ReadIRegReq
+         | Error (p,e) -> modError
 
     | FunctionCode.WriteDO ->
-      WriteDoRequest.TryParse pdu 
-      |> function 
-         | Ok x -> x |> WriteDOReq 
+      WriteDoRequest.TryParse pdu
+      |> function
+         | Ok x -> x |> WriteDOReq
          | Error (p,e) -> modError
 
-    | FunctionCode.WriteReg -> 
+    | FunctionCode.WriteReg ->
       WriteRegRequest.TryParse pdu
-      |> function 
-         | Ok x -> x |> WriteRegReq 
+      |> function
+         | Ok x -> x |> WriteRegReq
          | Error (p,e) -> modError
 
     | FunctionCode.WriteDOs ->
       WriteDosRequest.TryParse pdu
       |> function
-         | Ok x -> x |> WriteDOsReq 
+         | Ok x -> x |> WriteDOsReq
          | Error (p,e) -> modError
 
-    | FunctionCode.WriteRegs -> 
+    | FunctionCode.WriteRegs ->
       WriteRegsRequest.TryParse pdu
-      |> function 
-         | Ok x -> x |> WriteRegsReq 
+      |> function
+         | Ok x -> x |> WriteRegsReq
          | Error (p,e) -> modError
 
-  | Error _ -> 
+  | Error _ ->
     {
       FunctionCode = Invalid
-      ExceptionCode = IllegalFunction 
+      ExceptionCode = IllegalFunction
     } |> ModErrorReq
 
 
-let mapReqToRes (funcs : ModFuncs) (req : Request) : Response = 
+let mapReqToRes (funcs : ModFuncs) (req : Request) : Response =
   match req with
   | ReadDOReq x -> funcs.ReadDOFunc x |> ReadDORes
   | ReadDIReq x -> funcs.ReadDIFunc x |> ReadDIRes
@@ -115,34 +115,35 @@ let mapReqToRes (funcs : ModFuncs) (req : Request) : Response =
   | WriteDOsReq x -> funcs.WriteDOsFunc x |> WriteDOsRes
   | ModErrorReq x -> funcs.ModErrorFunc x |> ModErrorRes
 
-open Hopac 
+open Hopac
 open System.Net
 open System.Net.Sockets
-let rec handleReceive (slaveId : byte) (handle : Socket) (funcs : ModFuncs) : Job<unit> = 
+let rec handleReceive (slaveId : byte) (handle : Socket) (funcs : ModFuncs) : Job<unit> =
   job {
     let buff : Memory<byte> = Memory(Array.zeroCreate(300))
     let! len = handle.ReceiveAsync(buff, SocketFlags.None, CancellationToken(false)).AsTask()
-    match handle.Connected && len <> 0 with 
-      | true -> 
+    match handle.Connected && len <> 0 with
+      | true ->
         let frame = buff.Slice(0, len).ToArray()
-        let (pdu, mbap) = validateMbap slaveId frame 
+        let (pdu, mbap) = validateMbap slaveId frame
         let request = validatePdu pdu
+        printfn "%A" request
         let response = request |> mapReqToRes funcs
         let resPdu = response.Serialize()
-        let rawRes = Mbap.wrapPdu mbap resPdu |> List.toArray 
+        let rawRes = Mbap.wrapPdu mbap resPdu |> List.toArray
         let outBuff = rawRes |> ReadOnlyMemory
         let! _ = handle.SendAsync(outBuff, SocketFlags.None, CancellationToken()).AsTask()
         do! handleReceive slaveId handle funcs
-      | false -> 
+      | false ->
         handle.Disconnect(true)
         handle.Dispose()
   } |> Job.startIgnore
 
-let server (conf : ModbusServerConf) (actionFuncs : ModFuncs) : Job<unit> = 
+let server (conf : ModbusServerConf) (actionFuncs : ModFuncs) : Job<unit> =
   let listener = new Socket(SocketType.Stream, ProtocolType.Tcp)
   listener.Bind(IPEndPoint(conf.IPAddress, int conf.Port))
   listener.Listen(100)
   job {
     let! handler = listener.AcceptAsync()
     do! handleReceive conf.SlaveId handler actionFuncs
-  } |> Job.foreverIgnore 
+  } |> Job.foreverIgnore
--- readme.md
+++ readme.md
@@ -1,46 +1,48 @@
-commit fdaa9eb8353543c7448dde68f712073e814fd519
-Author: Bradley Phillips <bradder555@users.noreply.github.com>
-Date:   Sun Mar 17 22:40:01 2019 +0800
+# Introduction
+I did not like the available modbus libraries, i figured 'how hard can
+it be?' so i've decided to write it using fsharp and dotnet core
 
-    milestone - reads and writes digital IO
-    
-    I did not like the available modbus libraries, i figured 'how hard can
-    it be?' so i've decided to write it using fsharp and dotnet core
-    
-    This has been developed on linux, although it should also run on
-    windows (test someone?)
-    
-    Reading and writing of digital inputs and outputs is now successful
-    
-    The utility functions have ~100% test coverage - all of this is fairly
-    trivial, but it gets confusing fast! better to test, so behaviour is
-    documented also
-    
-    I think the digital IO is fairly robust, although i'd like to include
-    more testing, reading and writing the registers is still untested (and
-    probably incorrect)
-    
-    So far it's not a proper library, but it wouldn't be hard for me to
-    create a library project, which can be imported by the example program
-    (which includes the tests)
-    
-    It can get a bit confusing, getting one's head around byte orders,
-    what i have adopted in my code is, the head of the list should be the
-    least-significant, i.e. a uint of 258 should be converted to [2uy; 1uy]
-    
-    keeping 'my-side' in this format makes testing and understanding a lot
-    easier
-    
-    I just need to remember to re-organise when serializing the modbus
-    responses
-    
-    - develop and test the reading and writing of registers
-    - handle out-of-bounds exceptions
-    - develop and test the handling of receiving 'junk'
-    - handle any other modbus-spec exceptions
-    - convert to a library with examples
-    - write a client interface
-    - add tests for
-      - serialization,
-      - deserialization,
-      - and exception handling
+This has been developed on x86_64 Ubuntu, it should run on anything with 
+a NIC and can run the dotnetcore 2.2 run time (so ARM, x86), (Windows, Raspbian..)
+
+## Project Status
+The following has been implemented and is working
+- FC 1, Read Coils 
+- FC 2, Read Inputs
+- FC 3, Read Holding Registers 
+- FC 5, Write a Single Coil
+- FC 6, Write a Single Holding Register 
+- FC 15, Write multiple coils
+- FC 16, Write multiple holding registers 
+
+FC 4 has not been confirmed yet, once the deserialization testing has been
+added, i will be confident that this component is working.
+
+Failure to parse any of the function codes is handled and an error will be
+responded, however this is generic and will need more work 
+
+## Testing Status
+A heap of utility functions were written to assist with all of the bit,
+byte and word manipulations. 
+
+The utility functions have ~100% test coverage - all of this is fairly
+trivial, but it gets confusing fast! better to test, so behaviour is
+documented also
+
+## Philosophy / Design decisions
+It can get a bit confusing, getting one's head around byte orders,
+what i have adopted in my code is, the head of the list should be the
+least-significant, i.e. a uint of 258 should be converted to [2uy; 1uy]
+
+What is important is making the decision and sticking with it
+
+## Todo:
+- handle out-of-bounds and other deserialization exceptions
+- develop and test the handling of receiving 'junk' (leads on from the first point)
+- handle any other modbus-spec exceptions
+- write a client interface
+- add tests for
+  - deserialization,
+  - and exception handling
+- add round-trip testing
+- add integration testing
