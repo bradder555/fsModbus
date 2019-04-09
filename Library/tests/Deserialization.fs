f2b820b6e6bf5d07beeedbb2a4851834581c4439
brad <bradder555@users.noreply.github.com>
2019-04-09 14:18:50 +0800

    Adds ReadIRegRequest parsing tests

--- Library/tests/Deserialization.fs
+++ Library/tests/Deserialization.fs
@@ -236,4 +236,57 @@ let tests =
           Expect.equal exT exT1 "We are expecting a Match Failure Exception"
         }        
       ]
+
+      testList "ReadIRegRequest" [
+        test "parse ok" {
+          let payload = [4uy; 2uy; 4uy; 4uy; 1uy]
+          let e : ReadIRegRequest = {
+              Offset = 0x0204us
+              Quantity = 0x0401us
+            } 
+
+          let e : Result<ReadIRegRequest, PDU * exn> = e |> Ok
+          
+          let t = ReadIRegRequest.TryParse payload
+          Expect.equal e t "Valid ReadDoRequest "
+        }
+
+
+        test "parse fail short" {
+          let payload = [4uy; 2uy; 4uy;]
+          let t = ReadIRegRequest.TryParse payload
+          Expect.isError t "Should be error"
+          
+          let (pdu, exT) = 
+            t 
+            |> function 
+               | Result.Error x ->  x 
+               | _ -> [], Exception()
+          
+          Expect.equal pdu payload "the pdu should be returned"
+          
+          let exT = exT.GetType()
+          let exT1 = MatchFailureException("",0,0).GetType()
+          Expect.equal exT exT1 "We are expecting a Match Failure Exception"
+        }
+
+        test "parse fail long" {
+          let payload = [4uy; 2uy; 4uy; 2uy; 2uy; 5uy]
+          let t = ReadDiRequest.TryParse payload
+          Expect.isError t "Should be error"
+          
+          let (pdu, exT) = 
+            t 
+            |> function 
+               | Result.Error x ->  x 
+               | _ -> [], Exception()
+          
+          Expect.equal pdu payload "the pdu should be returned"
+          
+          let exT = exT.GetType()
+          let exT1 = MatchFailureException("",0,0).GetType()
+          Expect.equal exT exT1 "We are expecting a Match Failure Exception"
+        }        
+      ]
+
     ]
