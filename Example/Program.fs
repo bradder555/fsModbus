3ebbd506653a31b7c1c844f0483db6565a29a036
Bradley Phillips <bradder555@users.noreply.github.com>
2019-04-24 01:14:09 +0800

    feat: The library is MBap Request and Response focused
    The library is unopinionated about how to work with modbus
    
    The user should supply a delegate of MBapRequest to Result<MBapResponse,
    ()>
    
    This can probably be more useful to use exception, etc, but there's
    enough here to get started.
    
    What i don't like about this library is i can't find a way to keep the
    memory usage down, on my laptop (ubuntu with 16GB ram) the debug version
    of the program will use up to 1.7GB. I know this business-as-usual with
    dotnet but i can see no reason why the ram usage is so high!
    
    I was smashing the server with up to 400 requests per second (reading
    and writing) and CPU usage was well below 10%.
    
    I would like to compile this for and run it on a raspberry pi, i think
    this could work very nicely with an rPi
    
    The next step is to write a basic client, since all of the serialization
    and deserialization has been taken care of this should be fairly simple.
    
    I will try to make smaller, better documented commits
    
    ToDo:
    Write the client
    Restore testing
    Improve 'exception handling'
    Add logging
    Add round-trip testing

--- Example/Program.fs
+++ Example/Program.fs
@@ -9,10 +9,10 @@ open System
 let main argv =
   let shouldTest = argv |> Seq.tryFind (fun x -> x.ToLower() = "test") |> Option.isSome
   let runApp = argv |> Seq.tryFind (fun x -> x.ToLower() = "run") |> Option.isSome
-  
-  let conf = 
-    argv 
-    |> Seq.tryFind (fun x -> x.ToLower().Contains("binding")) 
+
+  let conf =
+    argv
+    |> Seq.tryFind (fun x -> x.ToLower().Contains("binding"))
     |> Option.map (fun x -> x.Split('=') |> Seq.last)
     |> Option.defaultValue "tcp://127.0.0.1:5502"
     |> ModbusTypes.ModbusServerConf.TryParse
@@ -22,8 +22,8 @@ let main argv =
   let mutable hReg = [0..100] |> List.map(fun x -> x, 0us) |> Map.ofList
   let mutable iReg = [0..100] |> List.map(fun x -> x, 0us) |> Map.ofList
 
-  let readHRegFunc (x : ReqOffQuant) : ResRegs = 
-    let b = x.Offset |> int
+  let readHRegFunc (x : ReqOffQuant) : ResRegs =
+    let b = x.Address |> int
     let c = x.Quantity |> int
     let e = b + c - 1
     let values : UInt16 list = [b..e] |> List.map (fun x -> Map.find x hReg )
@@ -31,20 +31,20 @@ let main argv =
       Values = values
     }
 
-  let writeRegFunc (x : WriteRegRequest) : WriteRegResponse = 
-    let o = x.Offset |> int
+  let writeRegFunc (x : WriteRegRequest) : WriteRegResponse =
+    let o = x.Address |> int
 
     hReg
     |> Map.add o x.Value
     |> fun y -> hReg <- y
 
     {
-      Offset = x.Offset
+      Address = x.Address
       Value = hReg |> Map.find o
     }
 
-  let writeRegsFunc (x : WriteRegsRequest) : ResOffQuant = 
-     let o = x.Offset |> int
+  let writeRegsFunc (x : WriteRegsRequest) : ResOffQuant =
+     let o = x.Address |> int
      let c = x.Quantity |> int
      let vals = x.Values
      [0..(c-1)]
@@ -52,11 +52,11 @@ let main argv =
      |> ignore
      {
        Quantity = x.Quantity
-       Offset = x.Offset
+       Address = x.Address
      }
 
-  let readDOFunc (x : ReqOffQuant) : ResBools = 
-    let offset = x.Offset |> int
+  let readDOFunc (x : ReqOffQuant) : ResBools =
+    let offset = x.Address |> int
     let count = x.Quantity |> int
     let ``end`` = offset + count - 1
     let values : bool list = [offset..``end``] |> List.map (fun x -> Map.find x dos)
@@ -64,64 +64,82 @@ let main argv =
       Status = values
     }
 
-  let writeDOFunc (x : WriteDoRequest) : WriteDoResponse = 
-    let start = x.Offset |> int
-    dos 
+  let writeDOFunc (x : WriteDoRequest) : WriteDoResponse =
+    let start = x.Address |> int
+    dos
     |> Map.add start x.Value
-    |> fun x -> dos <- x      
-    {      
-      Offset = x.Offset       
+    |> fun x -> dos <- x
+    {
+      Address = x.Address
       Value = dos |> Map.find start
     }
 
-  let writeDOsFunc (x : WriteDosRequest) : ResOffQuant = 
-    let offset = x.Offset |> int
+  let writeDOsFunc (x : WriteDosRequest) : ResOffQuant =
+    let offset = x.Address |> int
     let qty = x.Quantity |> int
     let vals = x.Values
-    [0..(qty-1)] 
+    [0..(qty-1)]
     |> List.map(fun x -> dos |> Map.add (offset + x) (vals |> List.item x) |> fun x -> dos <- x) // hacky!
     |> ignore
     {
-      Offset = x.Offset
+      Address = x.Address
       Quantity = x.Quantity
     }
 
-  let actionFunc : ModFunc = 
-    let r (req : Request) : Response =
-      match req with 
+  let actionFunc : ModFunc =
+    let r (req : RtuRequest) : RtuResponse =
+      match req with
       | ReadDOReq x -> readDOFunc x |> ReadDORes
-      | ReadDIReq x -> ModFunc.Default.Success.readDIFunc x 
+      | ReadDIReq x -> ModFunc.Default.Success.readDIFunc x
       | ReadHRegReq x -> readHRegFunc x |> ReadHRegRes
-      | ReadIRegReq x -> ModFunc.Default.Success.readIRegFunc x 
+      | ReadIRegReq x -> ModFunc.Default.Success.readIRegFunc x
       | WriteDOReq x -> writeDOFunc x |> WriteDORes
       | WriteRegReq x -> writeRegFunc x |> WriteRegRes
       | WriteDOsReq x -> writeDOsFunc x |> WriteDOsRes
-      | WriteRegsReq x -> ModFunc.Default.Success.writeRegsFunc x
+      | WriteRegsReq x -> writeRegsFunc x |> WriteRegsRes
     r
 
-  let conf = 
-    conf |> function | Ok x -> x // okay to crash here
-  
-  let modServer = Modbus.server conf actionFunc
+  let actionFunc : MbapFunc =
+    let r (req : MbapReq ) : Result<MbapRes, unit> =
 
-  let app = 
-    match runApp, shouldTest with 
-    | true, _ -> 
-      modServer >>- (fun x -> 0)
-    | false, false -> 
-      modServer >>- (fun x -> 0)
-    | _ -> 
+      match req.UnitIdentifier with
+      | 0uy ->
+        // we're only interested in one unit, which is '0',
+        // although we can have as many units as we want
+        let res =
+          req.Request
+          |> actionFunc
+
+        {
+          TransactionIdentifier = req.TransactionIdentifier
+          ProtocolIdentifier = req.ProtocolIdentifier
+          UnitIdentifier = req.UnitIdentifier
+          Response = res
+        } |> Ok
+      | _ -> () |> Error
+    r
+
+
+  let app =
+    match runApp, conf, shouldTest with
+    | true, Ok conf, _ ->
+      let modServer = Modbus.server conf actionFunc
+      modServer >>- (fun () -> 0)
+    | false, Ok conf, false ->
+      let modServer = Modbus.server conf actionFunc
+      modServer >>- (fun () -> 0)
+    | _ ->
       0 |> Job.result
 
   let tests =
     match shouldTest with
-    | true -> 
+    | true ->
       Tests.runTests
-    | false -> 
+    | false ->
       0 |> Job.result
 
-  [app; tests] 
+  [app; tests]
   |> Job.conCollect
-  |> Hopac.run 
-  |> Seq.tryFind (fun x -> x <> 0) 
+  |> Hopac.run
+  |> Seq.tryFind (fun x -> x <> 0)
   |> Option.defaultValue 0
--- Library/Util.fs
+++ Library/Util.fs
@@ -1,22 +1,26 @@
 module Util
 // for my code, the head should always be the least significant
 
-open System 
+open System
+#nowarn "25"
+// okay to crash if the pattern doesn't match,
+// may be better to take a byte list and attempt the pattern matching inside a try-with?
+// returning a result type..
 
-let tU32 [a; b; c; d] = BitConverter.ToUInt32([|a; b; c; d|],0)
-let tI32 [a; b; c; d] = BitConverter.ToInt32([|a; b; c; d|],0)
+let tU32 (a :: b :: c :: [d]) = BitConverter.ToUInt32([|a; b; c; d|],0)
+let tI32 (a :: b :: c :: [d]) = BitConverter.ToInt32([|a; b; c; d|],0)
 
-let tU16 [a; b] = BitConverter.ToUInt16([|a; b|],0)
-let tI16 [a; b] = BitConverter.ToInt16([|a; b|],0)
+let tU16 (a :: [b]) = BitConverter.ToUInt16([|a; b|],0)
+let tI16 (a :: [b]) = BitConverter.ToInt16([|a; b|],0)
 
 let byteToBool (x : byte) =
   [0..7] |> List.map (fun y -> x >>> y &&& 1uy = 1uy)
 
-let bytesToBool (x : byte list) = 
+let bytesToBool (x : byte list) =
   x |> List.fold (fun x y -> x @ byteToBool y ) []
 
-let bytesToUint16 (x : byte list) = 
-  List.foldBack (fun x (l,r) -> x::r, l) x ([],[]) 
+let bytesToUint16 (x : byte list) =
+  List.foldBack (fun x (l,r) -> x::r, l) x ([],[])
   |> fun (high,low) -> List.zip low high
   |> List.map (fun (high,low) -> [low; high] |> tU16)
 
@@ -29,44 +33,44 @@ let BoolToByte (x : bool) =
 
 // the resulting list should have the lowest value
 // bytes at head, i.e. [1..32] -> [1->8]@[9->16]@[17->24]@[25->32]
-let BoolsToBytes (bList : bool list) : byte list = 
+let BoolsToBytes (bList : bool list) : byte list =
   let byteCount = ((bList |> List.length) - 1) / 8 + 1
-  [1..byteCount] 
+  [1..byteCount]
   |> List.map (
-       fun x -> 
+       fun x ->
          let byteIndexes = [0..7]
-         
-         let a = 
+
+         let a =
            byteIndexes
-           |> List.map (fun y -> (x - 1) * 8 + y, y) 
-         
-         let b = 
-           a 
+           |> List.map (fun y -> (x - 1) * 8 + y, y)
+
+         let b =
+           a
            |> List.map (fun (x,y) -> (bList |> List.tryItem(x) |> function | Some x -> x | _ -> false),y)
-         
+
          let c =
            b
            |> List.fold (fun a (n, i) -> (BoolToByte n) <<< i |> (+) a) 0uy
-          
+
          c
     )
 
-let U16ToBytes (x : UInt16) : byte list = 
+let U16ToBytes (x : UInt16) : byte list =
   let b1 = x |> byte
   let b2 = (x - (b1 |> uint16)) >>> 8 |> byte
   [b1; b2]
 
-let U16sToBytes : UInt16 list -> byte list = 
+let U16sToBytes : UInt16 list -> byte list =
   List.map U16ToBytes >> List.concat
 
-let BoolToUint16 : bool -> UInt16 = 
+let BoolToUint16 : bool -> UInt16 =
   function | false -> 0us | true -> 255us <<< 8
-  
-let swapU16s (x : byte list) : byte list = 
+
+let swapU16s (x : byte list) : byte list =
   let len = (x |> List.length) / 2
   [0..len-1]
   |> List.map (
-       fun y -> 
+       fun y ->
           let a = x |> List.item (y * 2 )
           let b = x |> List.item (y * 2 + 1)
           a,b
